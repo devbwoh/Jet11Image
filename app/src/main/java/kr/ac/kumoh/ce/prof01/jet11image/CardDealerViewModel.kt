141f4a5d8fb7a78cc6afe87e098f4a2337185ae4
devbwoh <bwoh@outlook.kr>
2024-01-21 07:18:22 +0900

    🏗️ ViewModel 추가

--- app/src/main/java/kr/ac/kumoh/ce/prof01/jet11image/CardDealerViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet11image/CardDealerViewModel.kt
@@ -1,7 +1,49 @@
 package kr.ac.kumoh.ce.prof01.jet11image
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 
 class CardDealerViewModel : ViewModel() {
+    private val _cards = mutableStateListOf<Int>(-1, -1, -1, -1, -1)
+    //val cards: SnapshotStateList<Int>
+    val cards: List<Int> = _cards
 
+    fun shuffleCards() {
+        _cards.clear()
+        repeat(5) {
+            var num: Int
+            do {
+                num = (0 until 52).random()
+            } while (_cards.contains(num))
+            _cards.add(num)
+        }
+        _cards.sort()
+    }
+
+    fun getCardName(c: Int): String {
+        if (c < 0)
+            return "c_red_joker"
+
+        val shape = when (c / 13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> return "c_red_joker"
+        }
+
+        val number = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> return "c_red_joker"
+        }
+
+        return if (c % 13 in 10..12)
+            "c_${number}_of_${shape}2"
+        else
+            "c_${number}_of_${shape}"
+    }
 }
--- app/src/main/java/kr/ac/kumoh/ce/prof01/jet11image/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet11image/MainActivity.kt
@@ -1,16 +1,27 @@
 package kr.ac.kumoh.ce.prof01.jet11image
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import kr.ac.kumoh.ce.prof01.jet11image.ui.theme.Jet11ImageTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,16 +36,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen() {
+fun MainScreen(viewModel: CardDealerViewModel = viewModel()) {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Image(painter = painterResource(
-            id = R.drawable.c_3_of_hearts),
-            contentDescription = "카드 이미지"
-        )
+        Column {
+            CardImage(card = viewModel.cards[4])
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                onClick = {
+                    viewModel.shuffleCards()
+                }
+            ) {
+                Text("Good Luck", fontSize = 30.sp)
+            }
+
+        }
     }
+}
+
+@SuppressLint("DiscouragedApi")
+@Composable
+fun ColumnScope.CardImage(card: Int, viewModel: CardDealerViewModel = viewModel()) {
+    val id = LocalContext.current.resources.getIdentifier(
+        viewModel.getCardName(card),
+        "drawable",
+        LocalContext.current.packageName
+    )
+
+    Image(
+        painter = painterResource(id = id),
+        contentDescription = "카드 이미지",
+        modifier = Modifier.weight(1F).fillMaxSize()
+
+    )
 }
 
 @Preview(showBackground = true)
