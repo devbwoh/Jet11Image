f30523448c7b8e8a5ff339e56d4f211b03dec0b4
devbwoh <bwoh@outlook.kr>
2024-01-21 07:28:11 +0900

    💄 가로 화면 완성

--- app/src/main/java/kr/ac/kumoh/ce/prof01/jet11image/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet11image/MainActivity.kt
@@ -1,12 +1,15 @@
 package kr.ac.kumoh.ce.prof01.jet11image
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,6 +19,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,7 +46,7 @@ fun MainScreen(viewModel: CardDealerViewModel = viewModel()) {
         color = MaterialTheme.colorScheme.background
     ) {
         Column {
-            CardImage(card = viewModel.cards[4])
+            CardImageSection()
             Button(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -58,9 +62,25 @@ fun MainScreen(viewModel: CardDealerViewModel = viewModel()) {
     }
 }
 
+@Composable
+fun ColumnScope.CardImageSection() {
+    val viewModel: CardDealerViewModel = viewModel()
+
+    if (LocalConfiguration.current.orientation
+            == Configuration.ORIENTATION_LANDSCAPE) {
+        Row(Modifier.weight(1F)) {
+            viewModel.cards.map {
+                CardImage(it)
+            }
+        }
+    }
+    else
+        Text("세로")
+}
+
 @SuppressLint("DiscouragedApi")
 @Composable
-fun ColumnScope.CardImage(card: Int, viewModel: CardDealerViewModel = viewModel()) {
+fun RowScope.CardImage(card: Int, viewModel: CardDealerViewModel = viewModel()) {
     val id = LocalContext.current.resources.getIdentifier(
         viewModel.getCardName(card),
         "drawable",
@@ -70,14 +90,24 @@ fun ColumnScope.CardImage(card: Int, viewModel: CardDealerViewModel = viewModel(
     Image(
         painter = painterResource(id = id),
         contentDescription = "카드 이미지",
-        modifier = Modifier.weight(1F).fillMaxSize()
-
+        modifier = Modifier
+            .weight(1F)
+            .fillMaxSize()
+            .padding(4.dp)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MainScreenPreview() {
+    Jet11ImageTheme {
+        MainScreen()
+    }
+}
+
+@Preview(showBackground = true, device = "spec:width=1280dp,height=800dp,dpi=240")
+@Composable
+fun MainScreenLandscapePreview() {
     Jet11ImageTheme {
         MainScreen()
     }
